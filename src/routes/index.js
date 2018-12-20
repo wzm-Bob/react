249130f826eb7bfc49d260d58497564cf2e579d9
wangzm <1341591227@qq.com>
2018-12-20 18:28:01 +0800

    sdfg

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
+/**
+ * Created by 叶子 on 2017/8/13.
+ */
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-// import * as AllComponents from '../components';
-import Analysis from '../page/dashboard/analysis';
-//import routesConfig from './config';
-//import queryString from 'query-string';
+import AllComponents from '../components';
+import routesConfig from './config';
+import queryString from 'query-string';
 
 export default class CRouter extends Component {
-  /*   requireAuth = (permission, component) => {
-        debugger
+    requireAuth = (permission, component) => {
         const { auth } = this.props;
         const { permissions } = auth.data;
         // const { auth } = store.getState().httpData;
@@ -15,18 +16,50 @@ export default class CRouter extends Component {
         return component;
     };
     requireLogin = (component, permission) => {
-        debugger
         const { auth } = this.props;
         const { permissions } = auth.data;
         if (process.env.NODE_ENV === 'production' && !permissions) { // 线上环境判断是否登录
             return <Redirect to={'/login'} />;
         }
         return permission ? this.requireAuth(permission, component) : component;
-    }; */
+    };
     render() {
         return (
             <Switch>
-                <Route path='/home' component={Analysis}></Route>
+                {
+                    Object.keys(routesConfig).map(key => 
+                        routesConfig[key].map(r => {
+                            const route = r => {
+                                const Component = AllComponents[r.component];
+                                return (
+                                    <Route
+                                        key={r.route || r.key}
+                                        exact
+                                        path={r.route || r.key}
+                                        render={props => {
+                                            const reg = /\?\S*/g;
+                                            // 匹配?及其以后字符串
+                                            const queryParams = window.location.hash.match(reg);
+                                            // 去除?的参数
+                                            const { params } = props.match;
+                                            Object.keys(params).forEach(key => {
+                                                params[key] = params[key] && params[key].replace(reg, '');
+                                            });
+                                            props.match.params = { ...params };
+                                            const merge = { ...props, query: queryParams ? queryString.parse(queryParams[0]) : {} };
+                                            return r.login 
+                                                ? <Component {...merge} />
+                                                : this.requireLogin(<Component {...merge} />, r.auth)
+                                        }}
+                                    />
+                                )
+                            }
+                            return r.component ? route(r) : r.subs.map(r => route(r));
+                        })
+                    )
+                }
+
+                <Route render={() => <Redirect to="/404" />} />
             </Switch>
         )
     }
